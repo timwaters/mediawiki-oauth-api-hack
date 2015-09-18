3da148076cf72896d8f4d91d0b0ac8c3ccd9758c
Tim Waters <chippy2005@gmail.com>
2015-09-18 17:37:46 +0100

    basic search and replace for a specific atrribute in the map template

--- basic.rb
+++ basic.rb
@@ -52,32 +52,92 @@ body = JSON.parse(resp.body)
 # page name for the users user talk page
 page = "User talk:" + body["query"]["userinfo"]["name"]
 
-#
-# Next fetch the edit csrf token
-#
-uri = "#{site}/w/api.php?action=query&meta=tokens&type=csrf&format=json"
+#get the text of the users page
+uri = "#{site}/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=#{page}"
+#uri = "#{site}/w/api.php?action=query&prop=revisions&rvprop=content&format=json&pageids=51031"
 resp = @access_token.get(URI.encode(uri))
 body = JSON.parse(resp.body)
-puts body.inspect
-#{"batchcomplete"=>"", "query"=>{"tokens"=>{"csrftoken"=>"foobar"}}}
 
-token = body["query"]["tokens"]["csrftoken"]
+#puts body["query"]["pages"].inspect
 
-uri = "#{site}/w/api.php"
+pageid = body["query"]["pages"].keys.first
 
-post_body =  { "action" => "edit",
-                "title"=> page,
-                "section" => "new",
-                "sectiontitle" => "Hello World",
-                "text" => "this message was automtically posted by oauth demo script",
-                "summary" => "Hello world posting from oauth",
-                "watchlist" => "nochange",
-                "token" => token,
-                "format" => "json"
-              }
+revision = body["query"]["pages"][pageid]["revisions"].first 
+puts revision.inspect
+wikitext = revision["*"]
+puts wikitext
 
-resp = @access_token.post(URI.encode(uri), post_body)
+if wikitext.include? "{{Map"
+  # \{\{Map(.*?)\}\}  matches {{Map .... }}
+  #\{\{\s*Map(.*?)\}\} matches {{  Map .... }}
+end
+puts "-----"
 
-puts resp.body.inspect
-# "{\"edit\":{\"result\":\"Success\",\"pageid\":51031,\"title\":\"User talk:Chippyy\",
-# \"contentmodel\":\"wikitext\",\"oldrevid\":78186,\"newrevid\":78206,\"newtimestamp\":\"2015-09-18T14:52:01Z\"}}"
+map_match = /\{{2}\s*Map(.*?)\}{2}/m.match(wikitext)
+
+if map_match
+  map_template = map_match[0]
+  #split template by pipe symbols
+  map_attrs = map_template.split("|")
+  new_attrs = []
+  map_attrs.each do | map_attr |
+    if map_attr.include? "warped"
+      map_attr = "warped=yes \n"
+    end
+    new_attrs << map_attr
+  end
+  puts new_attrs.inspect
+  map_template = new_attrs.join("|")
+  puts map_template.inspect
+  
+  wikitext.sub!(/\{{2}\s*Map(.*?)\}{2}/m, map_template)
+  puts wikitext.inspect
+end
+
+
+
+
+
+
+# {"batchcomplete"=>"", "query"=>{"pages"=>{"51031"=>{"pageid"=>51031, "ns"=>3, 
+#   "title"=>"User talk:Chippyy", "revisions"=>[{"contentformat"=>"text/x-wiki", 
+#     "contentmodel"=>"wikitext", 
+#     "*"=>"{{Welcome|realName=|name=Chippyy}}\n\n-- [[User:Wikimedia Commons Welcome|Wikimedia Commons Welcome]] ([[User talk:Wikimedia Commons Welcome|<span class=\"signature-talk\">talk</span>]]) 14:17, 17 September 2015 (UTC)\n\n== Hello World ==\n\nthis message was automtically posted by oauth demo script\n\n== Hello World ==\n\nthis message was automtically posted by oauth demo script"}]}}}}
+
+if false
+
+  #
+  # Next fetch the edit csrf token
+  #
+  uri = "#{site}/w/api.php?action=query&meta=tokens&type=csrf&format=json"
+  resp = @access_token.get(URI.encode(uri))
+  body = JSON.parse(resp.body)
+  puts body.inspect
+  #{"batchcomplete"=>"", "query"=>{"tokens"=>{"csrftoken"=>"foobar"}}}
+
+  token = body["query"]["tokens"]["csrftoken"]
+
+  uri = "#{site}/w/api.php"
+  #"pageid" => 51031
+  post_body =  { "action" => "edit",
+                  "title"=> page,
+                  "section" => "new",
+                  "sectiontitle" => "Hello World",
+                  "text" => "this message was automtically posted by oauth demo script",
+                  "summary" => "Hello world posting from oauth",
+                  "watchlist" => "nochange",
+                  "bot"=> "true",
+                  "nocreate" => "true",
+                  "contentmodel" => "wikitext",
+                  "contentformat" => "text/plain",
+                  "token" => token,
+                  "format" => "json"
+                }
+  resp = @access_token.post(URI.encode(uri), post_body)
+  puts resp.body.inspect
+  # "{\"edit\":{\"result\":\"Success\",\"pageid\":51031,\"title\":\"User talk:Chippyy\",
+  # \"contentmodel\":\"wikitext\",\"oldrevid\":78186,\"newrevid\":78206,\"newtimestamp\":\"2015-09-18T14:52:01Z\"}}"
+end
+
+
+
